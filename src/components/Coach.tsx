ad879bc2e7d5ec67ad227e92931b59881fe07080
Grace-Rasaily780 <rasailygrace56@gmail.com>
2024-06-03 11:35:33 +0545

    second release

--- src/components/Coach.tsx
+++ src/components/Coach.tsx
@@ -1,56 +1,54 @@
-import styles from './Coach.module.css';
-import { Title, Text, Container, Button, Overlay } from '@mantine/core';
-import Image from 'next/image';
+import styles from "./Coach.module.css";
+import { Title, Text, Container, Button, Overlay } from "@mantine/core";
+import Image from "next/image";
 
 export default function Coach() {
   let coaches = [
-  {
-    img: '/about_photo.jpg',
-    name: 'John Doe 1',
-    post: 'Coach'
-  },
-  {
-    img: '/about_photo.jpg',
-    name: 'John Doe 2',
-    post: 'Coach'
-  },
-  {
-    img: '/about_photo.jpg',
-    name: 'John Doe 3',
-    post: 'Coach'
-  },
-
-  ]
+    {
+      img: "/about_photo.jpg",
+      name: "Sajan Theeng Tamang",
+      phone: "+977 9702339383",
+    },
+    {
+      img: "/about_photo.jpg",
+      name: "Suman Nepali ",
+      phone: "+977 9823725634",
+    },
+    {
+      img: "/about_photo.jpg",
+      name: "Roshan Achhami ",
+      phone: "+977 9845877861",
+    },
+  ];
   return (
     <div className={styles.container} id="coach">
-      <Title className={styles.title}>
-        Our Coaches 
-      </Title>
-          
-      <div className={styles.card_container}>
-        {
-          coaches.map(coach => (
-            <div className={styles.card} key={coach.name}>
-                           
-              <div className={styles.image_container}>
-                <Image
-                  alt="Image"
-                  src="/about_photo.jpg"
-                  fill={true}
-                  // objectFit="co ver" // Scale your image down to fit into the container
-                  className={styles.image}
-                />
-                </div>
-                <div className={styles.card_text}>
-                  <p className={styles.name}>{coach.name}</p>
-                  <p className={styles.post}>{coach.post}</p>
-                </div>
+      <Title className={styles.title}>Our Coaches</Title>
 
+      <div className={styles.card_container}>
+        {coaches.map((coach) => (
+          <div className={styles.card} key={coach.name}>
+            <div className={styles.image_container}>
+              <Image
+                alt={coach.name}
+                src="/about_photo.jpg"
+                fill={true}
+                // objectFit="co ver" // Scale your image down to fit into the container
+                className={styles.image}
+              />
             </div>
-          ))
-        }
-        
+            <div className={styles.card_text}>
+              <p className={styles.name}>{coach.name}</p>
+              <a
+                className={styles.post}
+                href={`tel:${coach.phone}`}
+                target="_blank"
+              >
+                {coach.phone}
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,87 +1,126 @@
-'use client';
-import styles from './Footer.module.css';
-import { Text, Container, ActionIcon, Group, rem } from '@mantine/core';
-import { IconBrandFacebook, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
-import classes from './Footer.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import { Text, Container, ActionIcon, Group, rem } from "@mantine/core";
+import {
+  IconBrandFacebook,
+  IconBrandYoutube,
+  IconBrandInstagram,
+} from "@tabler/icons-react";
+import classes from "./Footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const data = [
   {
-    title: 'Navigation',
+    title: "Navigation",
     links: [
-      { label: 'About', link: '/#about' },
-      { label: 'Services', link: '/#service' },
-      { label: 'Coaches', link: '/#coach' },
-      { label: 'Contact', link: '/#contact' },
-
+      { label: "About", link: "/#about" },
+      { label: "Services", link: "/#service" },
+      { label: "Coaches", link: "/#coach" },
+      { label: "Contact", link: "/#contact" },
     ],
   },
   {
-    title: 'Resources',
+    title: "Resources",
     links: [
-      { label: 'Fighters', link: '/fighters' },
-      { label: 'Membership Form', link: '/membership' },
+      { label: "Fighters", link: "/fighters" },
+      { label: "Membership Form", link: "/wordpress/membership" },
     ],
-  }
-  ];
+  },
+];
 
-export default function Footer(){
+export default function Footer() {
   const groups = data.map((group) => {
-  const links = group.links.map((link, index) => (
-    <Link
-      key={index}
-      className={classes.link}
-      href={link.link}
-      // onClick={(event) => event.preventDefault()}
-    >
-      {link.label}
-    </Link>
-  ));
+    const links = group.links.map((link, index) => (
+      <Link
+        key={index}
+        className={classes.link}
+        href={link.link}
+        // onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </Link>
+    ));
 
-  return (
-    <div className={classes.wrapper} key={group.title}>
-      <Text className={classes.title}>{group.title}</Text>
-      {links}
-    </div>
-  );
-});
+    return (
+      <div className={classes.wrapper} key={group.title}>
+        <Text className={classes.title}>{group.title}</Text>
+        {links}
+      </div>
+    );
+  });
   return (
     <footer className={classes.footer}>
       <Container className={classes.inner}>
         <div className={classes.logo}>
-          <Image 
+          <Image
             src="/logo_full.svg"
             width={200}
             height={120}
-            alt="Full logo"
+            alt="FightClub logo"
           />
           <Text size="lg" c="dimmed" className={classes.description}>
-          Unleash Your Inner Fighter
+            Unleash Your Inner Fighter
           </Text>
         </div>
         <div className={classes.groups}>{groups}</div>
       </Container>
       <Container className={classes.afterFooter}>
         <Text c="dimmed" size="sm">
-          © 2024 Fight Club Nepal. All rights reserved. | Made by Grace Rasaily
+          © 2024 Fight Club Nepal. All rights reserved. | Made by{" "}
+          <a href="https://github.com/Grace-Rasaily780" target="_blank">
+            Grace Rasaily
+          </a>
         </Text>
 
-        <Group gap={0} className={classes.social} justify="flex-end" wrap="nowrap">
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandFacebook style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+        <Group
+          gap={0}
+          className={classes.social}
+          justify="flex-end"
+          wrap="nowrap"
+        >
+          <ActionIcon
+            size="lg"
+            color="gray"
+            variant="subtle"
+            component="a"
+            href="https://www.facebook.com/profile.php?id=61550903675711"
+            target="_blank"
+          >
+            <IconBrandFacebook
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
           </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandYoutube style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+          <ActionIcon
+            size="lg"
+            color="gray"
+            variant="subtle"
+            component="a"
+            href="https://www.youtube.com/@FightClubNepal"
+            target="_blank"
+          >
+            <IconBrandYoutube
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
           </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+          <ActionIcon
+            size="lg"
+            color="gray"
+            variant="subtle"
+            component="a"
+            href="https://www.instagram.com/fightclub_nepal/"
+            target="_blank"
+          >
+            <IconBrandInstagram
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
           </ActionIcon>
         </Group>
       </Container>
-    </footer>  
-  )
+    </footer>
+  );
 }
-
 
 // <MantineLogo size={30} />
