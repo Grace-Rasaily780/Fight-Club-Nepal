cd633e27d56088a0740be519d93bc36c26367063
Grace-Rasaily780 <rasailygrace56@gmail.com>
2024-06-05 09:55:10 +0545

    Metadata updated open graph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Header from "../components/Header";
 import About from "../components/About";
-import Service from "../components/Service"
-import Coach from "../components/Coach"
+import Service from "../components/Service";
+import Coach from "../components/Coach";
 import Contact from "../components/Contact";
 
 export default function Home() {
   return (
     <>
-    <Header />
-    <About />
-    <Service />
-    <Coach />
-    <Contact />
+      <Header />
+      <About />
+      <Service />
+      <Coach />
+      <Contact />
     </>
   );
 }
